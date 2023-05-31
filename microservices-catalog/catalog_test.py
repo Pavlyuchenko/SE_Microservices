0e5789f78da1070cb354aa1717bcaad2a0aa72e4
Michal Pavlíček <michaelg.pavlicek@gmail.com>
2023-05-31 19:50:28 +0200

    Update catalog test example

--- microservices-catalog/catalog_test.py
+++ microservices-catalog/catalog_test.py
@@ -1,50 +1,25 @@
-import json
-import time
 import unittest
-from catalog import app
+from catalog import app, books
 
 app.testing = True
 
 
-class test(unittest.TestCase):
+class CatalogTest(unittest.TestCase):
+    example_book_data = {
+        "id": 1,
+        "title": "Harry Potter",
+        "author": "JK Rowling",
+        "pub_year": "1997",
+    }
+
     def test_no_title(self):
         with app.test_client() as client:
-            # send data as request.args to endpoint
-            book1_data = {
-                "id": 1,
-                "title": "Harry Potter",
-                "author": "JK Rowling",
-                "pub_year": "1997",
-            }
-            book2_data = {
-                "id": 2,
-                "title": "The Lord of the Rings",
-                "author": "JRR Tolkien",
-                "pub_year": "1954",
-            }
             # call the endpoint (query_string is the data that is sent as request.args)
-            res1 = client.post("/books", query_string=book1_data)
-            res2 = client.post("/books", query_string=book2_data)
-
-            # check if result is 200 == the call to the method was successful
-            self.assertEqual(res1.status_code, 200)
-            self.assertEqual(res2.status_code, 200)
-
-            # Get response from the GET books endpoint
-            book1_api = client.get("/books/1").text
-            book2_api = client.get("/books/2").text
-            # Converts data from string to dict
-            book1_api = json.loads(book1_api)
-            book2_api = json.loads(book2_api)
-
-            # Prints for explanation
-            print("Data inside GET /books/1:")
-            print(book1_api)
-            print("Data inside GET /books/2:")
-            print(book2_api)
+            client.post("/books", query_string=self.example_book_data)
 
             # Example of usage
-            self.assertEqual(book1_api["title"], book1_data["title"])
+            self.assertEqual(len(books), 1)
+            self.assertEqual(books[0].title, "Harry Potter")
 
 
 if __name__ == "__main__":
